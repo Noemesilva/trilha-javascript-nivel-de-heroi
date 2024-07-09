7bc12079669a5d999fd46dceb09dc0b0f05e834c
Noeme Silva <noeme16@gmail.com>
2024-07-09 12:25:18 -0300

    feat: Classificação de herói por XP

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-let nomeHeroi = "";
-let XP;
+let nomeHeroi = "Mario";
+let xpHeroi = 9800;
+
+
+function classificarHeroi(xp)
+{
+    switch(true)
+    {
+        case (xp < 1000):
+            
+            return "Ferro";
+
+            case (xp >=1001 && xp <=2000):
+            
+                return "Bronze";
+            
+            case (xp >=2001 && xp <= 5000):
+            
+                return "Prata";
+            
+            case(xp >= 5001 && xp <= 7000):
+            
+                return "Ouro";
+            
+            case (xp >= 7001 && xp <= 8000):
+            
+                return "Platina";
+            
+            case (xp >= 8001 && xp <= 9000):
+            
+               return "Ascendente";
+            
+           case (xp >= 9001 && xp <= 10000):
+            
+                return "Imortal";
+            
+             case (xp >= 10001):
+            
+              return "Radiante";
+    }
+}
+
+let nivelHeroi = classificarHeroi(xpHeroi);
+console.log(`O Herói de nome ${nomeHeroi} está no nivel de ${nivelHeroi}`);
+
+
 
